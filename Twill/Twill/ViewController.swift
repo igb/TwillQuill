b113ba1e2fc01630fad39ed99e988fe38985cb56
Ian Brown <igb@hccp.org>
2022-05-27 22:29:05 -0700

    working paint frame

---
+++ Twill/Twill/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Twill
+//
+//  Created by Ian Brown on 5/27/22.
+//
+
+import UIKit
+import PencilKit
+
+class ViewController:  UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+    @IBOutlet weak var canvasView: PKCanvasView!
+    var toolPicker: PKToolPicker!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    /// Set up the drawing initially.
+       override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+           
+           // Set up the canvas view with the first drawing from the data model.
+           canvasView.delegate = self
+         //  canvasView.drawing = dataModelController.drawings[drawingIndex]
+           canvasView.alwaysBounceVertical = true
+           
+           canvasView.frame(forAlignmentRect: self.view.frame)
+
+           
+           // Set up the tool picker
+           if #available(iOS 14.0, *) {
+               toolPicker = PKToolPicker()
+           } else {
+               // Set up the tool picker, using the window of our parent because our view has not
+               // been added to a window yet.
+               let window = parent?.view.window
+               toolPicker = PKToolPicker.shared(for: window!)
+           }
+           
+           toolPicker.setVisible(true, forFirstResponder: canvasView)
+           toolPicker.addObserver(canvasView)
+           toolPicker.addObserver(self)
+        //   updateLayout(for: toolPicker)
+           canvasView.becomeFirstResponder()
+       }
+
+
+    
+    
+    
+
+    
+    
+}
+
---
+++ Twill/TwillUITests/TwillUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwillUITestsLaunchTests.swift
+//  TwillUITests
+//
+//  Created by Ian Brown on 5/27/22.
+//
+
+import XCTest
+
+class TwillUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

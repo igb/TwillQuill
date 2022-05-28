8d3e47eeca8e801abef14eed0bf544063becad60
Ian Brown <igb@hccp.org>
2022-05-28 09:44:08 -0700

    capturing image from canvas and (for now) saving it to the photos app

--- Twill/Twill/ViewController.swift
+++ Twill/Twill/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController:  UIViewController, PKCanvasViewDelegate, PKToolPickerObser
     
     @IBAction func tweet(_ sender: UIButton) {
         NSLog("tweet")
+        UIImageWriteToSavedPhotosAlbum(canvasView.asImage(),nil,nil,nil);
+
+        
     }
 
     override func viewDidLoad() {
@@ -65,8 +68,32 @@ class ViewController:  UIViewController, PKCanvasViewDelegate, PKToolPickerObser
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
+    
+    
+    
 
     
     
+}
+
+
+extension UIView {
+
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
 }
 
